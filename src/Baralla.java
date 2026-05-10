f2ffacf83a4288adc33abafa2e8bc0e25b53a40d
aina <ainamrigo@gmail.com>
2026-05-10 20:00:55 +0200

    A la classe Baralla, afegir  l'array de numeros i colors del RummiKub, i creació del seu mètode per crear la seva baralla

--- src/Baralla.java
+++ src/Baralla.java
@@ -1,28 +1,35 @@
 import java.util.ArrayList;
 
 public class Baralla {
-    ArrayList <Carta> baralla = new ArrayList<Carta>();
-    final String[] numeros = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "AS"};
+    ArrayList<Carta> baralla = new ArrayList<Carta>();
+    final String[] numerosFrancesa = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "AS"};
     final String joker = "Joker";
-    final String[] palsNegres = {"Pica", "Trevol"};
-    final String[] palsVermells = {"Cor", "Diamant"};
+    final String[] palsFrancesa = {"Pica", "Trevol", "Cor", "Diamant"};
 
-    public void crearCartes() {
-        for (int pal = 0; pal < palsNegres.length; pal++) {
-            for ( int num = 0; num < numeros.length; num++){
-                new Carta(numeros[num], palsNegres[pal]);
-                baralla.add(new Carta(numeros[num], palsNegres[pal], "negre"));
+    final String[] numerosRummiKub = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"};
+    final String[] colorsRummiKub = {"Vermell", "Blau", "Groc", "Negre"};
+
+
+    public void crearCartesRummy() {
+        for (int pal = 0; pal < palsFrancesa.length; pal++) {
+            for (int num = 0; num < numerosFrancesa.length; num++) {
+                baralla.add(new Carta(numerosFrancesa[num], palsFrancesa[pal]));
             }
         }
 
-        for (int pal = 0; pal < palsVermells.length; pal++) {
-            for ( int num = 0; num < numeros.length; num++){
-                new Carta(numeros[num], palsVermells[pal]);
-                baralla.add(new Carta(numeros[num], palsVermells[pal], "vermell"));
+        baralla.add(new Carta(joker));
+        baralla.add(new Carta(joker));
+    }
+
+    public void crearCartesRummiKub() {
+        for (int i = 0; i < 2; i++) {
+            for (int color = 0; color < colorsRummiKub.length; color++) {
+                for (int num = 0; num < numerosRummiKub.length; num++) {
+                    baralla.add(new Carta(numerosRummiKub[num], colorsRummiKub[color]));
+                }
             }
         }
-        baralla.add(new Carta(joker, "negre"));
-        baralla.add(new Carta(joker, "vermell"));
-
+        baralla.add(new Carta(joker));
+        baralla.add(new Carta(joker));
     }
 }
