d2900836ee715bbce66a50b988c3deb5b8437081
aina <ainamrigo@gmail.com>
2026-05-18 21:16:15 +0200

    A la classe joc s'han afegit diferents mètodes per preparar la partida i repartir les cartes segons el num de jugadors i modalitat

--- src/Joc.java
+++ src/Joc.java
@@ -1,4 +1,11 @@
 public class Joc {
+    public static void prepararPartida(){
+        Baralla laMevaBaralla = new Baralla();
+        int nBaralles = Baralla.determinarQuantitatBaralles(modalitatJoc, numJugadors);
+        laMevaBaralla.inicialitzarBaralla(modalitatJoc, nBaralles);
+        laMevaBaralla.mesclarCartes();
+        repartirCartes(laMevaBaralla);
+    }
     static int modalitatJoc = Consola.triarModalitat();
     static int numJugadors = Consola.triarNumJugadors(modalitatJoc);
 
@@ -11,11 +18,43 @@ public class Joc {
         }
     }
 
-    public static  void main (String [] args) {
-        establirNomsJugadors();
-        System.out.println(arrayJugadors[0]);
-        System.out.println();
-        System.out.println(arrayJugadors[1]);
+
+    static int determinarCartesInicials(int modalitat, int numJugadors) {
+        int cartes = 7; // valor per defecte
+        switch (modalitat) {
+            case 1: // rummy classic
+                if (numJugadors == 2) cartes = 10;
+                else if (numJugadors == 5 || numJugadors == 6) cartes = 6;
+                break;
+            case 2: // Rummikub
+                cartes = 14;
+                break;
+            case 3: // Gin Rummy
+                cartes = 10;
+                break;
+            case 4: // Rummy Argentino
+                cartes = 13;
+                break;
+        }
+        return cartes;
     }
 
+    public static void repartirCartes(Baralla laMevaBaralla) {
+        int numCartes = determinarCartesInicials(modalitatJoc, numJugadors);
+
+        for (int i = 0; i < numCartes; i++) {
+
+            for (int j = 0; j < arrayJugadors.length; j++) {
+
+                if (laMevaBaralla.baralla.size() > 0) {
+
+                    Carta c = laMevaBaralla.baralla.get(0);
+
+                    arrayJugadors[j].maCartes.add(c);
+
+                    laMevaBaralla.baralla.remove(0);
+                }
+            }
+        }
+    }
 }
