b5023299d3b04418ff03c65ed849f4cd90bd0ce8
aina <ainamrigo@gmail.com>
2026-05-18 20:33:10 +0200

    A la classe consola s'ha modificat el metode que demana la modalitat del joc per inclourer-les totes, demanant num de jugadors i el nom de cada jugador

--- src/Consola.java
+++ src/Consola.java
@@ -7,27 +7,28 @@ public class Consola {
     public static void mostrarBaralla(ArrayList<Carta> cartes) {
         for (int i = 0; i < cartes.size(); i++) {
             Carta c = cartes.get(i);
-            System.out.println((i + 1)+ ". " + c.toString());
+            System.out.println((i + 1) + ". " + c.toString());
         }
     }
 
-    public static int triarModalitat(){
+    public static int triarModalitat() {
         int modalitat = 0;
         while (modalitat <= 0) {
-            System.out.println("Indica a quina modalitat vols jugar: '1': Rummy Argtenmtino, '2': GinRummy, '3': rummyKub");
+            System.out.println("Indica a quina modalitat vols jugar: '1': Rummy clàssic, '2': rummyKub GinRummy, '3': GinRummy, '4'; Rummy Argtenmtino");
             modalitat = scanner.nextInt();
             scanner.nextLine();
-            if (modalitat <= 0 || modalitat > 3) {
-                System.out.println("Modalitat de joc no v�lida, torna a triar");
+            if (modalitat <= 0 || modalitat > 4) {
+                System.out.println("Modalitat de joc no valida, torna a triar");
             }
         }
         return modalitat;
     }
-    public static int triarNumJugadors(int modalitatTriada){
-        int numJugadors = 1;
+
+    public static int triarNumJugadors(int modalitatTriada) {
+        int numJugadors = 0;
         if (modalitatTriada == 1) {
             while (numJugadors < 2 || numJugadors > 6) {
-                System.out.println("Introdueix el num de jugadors per al rummyArgentino, recorda: minim 2 i maxim 6");
+                System.out.println("Introdueix el num de jugadors per al Rummy clàssic, recorda: minim 2 i maxim 6");
                 numJugadors = scanner.nextInt();
                 scanner.nextLine();
                 if (numJugadors < 2 || numJugadors > 6) {
@@ -35,14 +36,23 @@ public class Consola {
                 }
             }
         } else if (modalitatTriada == 2) {
-            System.out.println("el num de jugadors per al GinRummy es 2");
-            numJugadors = 2;
-        } else if (modalitatTriada == 3) {
             while (numJugadors < 2 || numJugadors > 6) {
                 System.out.println("Introdueix el num de jugadors per al Rummikub, recorda: minim 2 i maxim 4");
                 numJugadors = scanner.nextInt();
                 scanner.nextLine();
                 if (numJugadors < 2 || numJugadors > 4) {
+                    System.out.println("Num de jugadors no valid");
+                }
+            }
+        } else if (modalitatTriada == 3) {
+            System.out.println("el num de jugadors per al GinRummy es 2");
+            numJugadors = 2;
+        } else if (modalitatTriada == 4) {
+            while (numJugadors < 2 || numJugadors > 6) {
+                System.out.println("Introdueix el num de jugadors per al Rummy Argentino, recorda: minim 2 i maxim 6");
+                numJugadors = scanner.nextInt();
+                scanner.nextLine();
+                if (numJugadors < 2 || numJugadors > 6) {
                     System.out.println("Num de jugadors no valid");
                 }
             }
