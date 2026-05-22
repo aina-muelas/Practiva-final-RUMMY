f47b5a9151f2402a2d096d7ea8a8f51780aeb2e1
aina <amuelas522@alumnes.politecnicllevant.cat>
2026-05-22 13:26:43 +0200

    Establir el valor(puntuacio) de la carta/fitxa

--- src/Baralla.java
+++ src/Baralla.java
@@ -4,10 +4,11 @@ import java.util.Collections;
 public class Baralla {
     ArrayList<Carta> baralla = new ArrayList<Carta>();
     final String[] numerosFrancesa = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "AS"};
+    final int[] valorsFrancesa = {1,2,3,4,5,6,7,8,9,10,10,10,10};
     final String joker = "Joker";
     final String[] palsFrancesa = {"Pica", "Trevol", "Cor", "Diamant"};
 
-    final String[] numerosRummiKub = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"};
+    // final String[] numerosRummiKub = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"};
     final String[] colorsRummiKub = {"Vermell", "Blau", "Groc", "Negre"};
 
 
@@ -26,25 +27,25 @@ public class Baralla {
     public void crearBarallaFrancesa( int modalitat) {
         for (int pal = 0; pal < palsFrancesa.length; pal++) {
             for (int num = 0; num < numerosFrancesa.length; num++) {
-                baralla.add(new Carta(numerosFrancesa[num], palsFrancesa[pal]));
+                baralla.add(new Carta(numerosFrancesa[num], palsFrancesa[pal], valorsFrancesa[num]));
             }
         }
         if (modalitat != 3) {
-            baralla.add(new Carta(joker, "Comodí"));
-            baralla.add(new Carta(joker, "Comodí"));
+            baralla.add(new Carta(joker, "Comodí", 0));
+            baralla.add(new Carta(joker, "Comodí", 0));
         }
     }
 
     public void crearCartesRummiKub() {
         for (int i = 0; i < 2; i++) {
             for (int color = 0; color < colorsRummiKub.length; color++) {
-                for (int num = 0; num < numerosRummiKub.length; num++) {
-                    baralla.add(new Carta(numerosRummiKub[num], colorsRummiKub[color]));
+                for (int num = 1; num <= 13; num++) {
+                    baralla.add(new Carta(String.valueOf(num), colorsRummiKub[color], num));
                 }
             }
         }
-        baralla.add(new Carta(joker, "Comodí"));
-        baralla.add(new Carta(joker, "Comodí"));
+        baralla.add(new Carta(joker, "Comodí", 0));
+        baralla.add(new Carta(joker, "Comodí", 0));
     }
 
     static int determinarQuantitatBaralles(int modalitat, int numJugadors){
--- src/Carta.java
+++ src/Carta.java
@@ -1,10 +1,12 @@
 public class Carta {
     String numero;
     String palOColor;
+    int valor;
 
-    public Carta(String numero, String palOColor){
+    public Carta(String numero, String palOColor, int valor){
         this.numero = numero;
         this.palOColor = palOColor;
+        this.valor = valor;
     }
 
     public Carta(String numero){
