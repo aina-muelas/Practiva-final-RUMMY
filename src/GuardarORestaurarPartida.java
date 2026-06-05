44320cf0443f2fa079ab50681de0bfb288d2e582
aina <ainamrigo@gmail.com>
2026-06-05 20:14:31 +0200

    S'ha acabat d'implementat la classe que GuardarORestaurarPartida, on s'han creat correctament els seus atributs necesaris, constructors, els 2 mètodes per guardar i renaudar, i els getters

--- src/GuardarORestaurarPartida.java
+++ src/GuardarORestaurarPartida.java
@@ -1,82 +1,86 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.FileNotFoundException;
-import java.io.ObjectInputStream;
+import java.io.*;
 import java.util.ArrayList;
 
 public class GuardarORestaurarPartida {
+    private String nomArxiu;
+    private int modalitat;
+    private int tornActual;
     private Jugador[] arrayJugadors;
     private ArrayList<ArrayList<Carta>> taulerComu;
     private Baralla baralla;
     private ArrayList<Carta> pilarDescartades;
 
-    public GuardarORestaurarPartida(Jugador[] arrayJugadors, ArrayList<ArrayList<Carta>> taulerComu, Baralla baralla, ArrayList<Carta> pilarDescartades) {
+    public GuardarORestaurarPartida(String nomArxiu, int modalitat, int torn, Jugador[] arrayJugadors, ArrayList<ArrayList<Carta>> taulerComu, Baralla baralla, ArrayList<Carta> pilarDescartades) {
+        this.nomArxiu = nomArxiu;
+        this.modalitat = modalitat;
+        this.tornActual = torn;
         this.arrayJugadors = arrayJugadors;
         this.taulerComu = taulerComu;
         this.baralla = baralla;
         this.pilarDescartades = pilarDescartades;
     }
 
+    public GuardarORestaurarPartida(String nomArxiu) {
+        this.nomArxiu = nomArxiu;
+    }
+
     public void guardarPartida(){
         try {
-            ObjectOutputStream escritor = new ObjectOutputStream(new FileOutputStream("d:/jocRummy.bin"));
-            //Escribimos la matriz tal cuál está en memoria RAM, como archivo binario
-            //escritor.writeObject(tablero);
-            escritor.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("No s'ha trobat l'arxiu: jocRummy.bin");
+            File carpeta = new File("partidesRummyGuardades");
+            if (!carpeta.exists()) {
+                carpeta.mkdir();
+            }
+            ObjectOutputStream fitxerSortida = new ObjectOutputStream(new FileOutputStream( "partidesRummyGuardades" + File.separator + nomArxiu + ".bin"));
+            fitxerSortida.writeObject(modalitat);
+            fitxerSortida.writeObject(tornActual);
+            fitxerSortida.writeObject(arrayJugadors);
+            fitxerSortida.writeObject(taulerComu);
+            fitxerSortida.writeObject(baralla);
+            fitxerSortida.writeObject(pilarDescartades);
+            fitxerSortida.close();
         } catch (IOException e) {
-            System.out.println("Problema d'acces a l'arxiu: jocRummy.bin");
+            System.out.println("Problema, no s'ha pogut guardar la partida amb el nom '"  + nomArxiu + ".bin'");
         }
     }
 
-    public void cargarPartida(){
+    public void restaurarPartida(){
         try {
-            ObjectInputStream lector = new ObjectInputStream(new FileInputStream("d:/jocRummy.bin"));
-            //Leemos los bytes del archivo y nos retornará un Object, al que haremos casting a matriz int[]
-            // tablero = (int[][]) lector.readObject();
-            lector.close();
+            ObjectInputStream fitxerEntrada = new ObjectInputStream(new FileInputStream("partidesRummyGuardades" + File.separator + nomArxiu + ".bin"));
+            this.modalitat = (int) fitxerEntrada.readObject();
+            this.tornActual = (int) fitxerEntrada.readObject();
+            this.arrayJugadors = (Jugador[]) fitxerEntrada.readObject();
+            this.taulerComu = (ArrayList<ArrayList<Carta>>) fitxerEntrada.readObject();
+            this.baralla = (Baralla) fitxerEntrada.readObject();
+            this.pilarDescartades = (ArrayList<Carta>) fitxerEntrada.readObject();
+            fitxerEntrada.close();
         } catch (FileNotFoundException e) {
-            System.out.println("No es troba el fitxer: jocRummy.bin");
+            System.out.println("No es troba el fitxer: " + nomArxiu + ".bin");
         } catch (IOException e) {
-            System.out.println("Problema d'acces a l'arxiu: jocRummy.bin");
+            System.out.println("Problema d'acces a l'arxiu: " + nomArxiu + ".bin");
         } catch (ClassNotFoundException e) {
-            System.out.println("LEs dades obtingudes no permeten recuperar la partida");
+            System.out.println("Les dades obtingudes no permeten recuperar la partida");
         }
     }
 
+    public int getModalitat() { return modalitat;}
 
     public Jugador[] getArrayJugadors() {
         return arrayJugadors;
-    }
-
-    public void setArrayJugadors(Jugador[] arrayJugadors) {
-        this.arrayJugadors = arrayJugadors;
     }
 
     public ArrayList<ArrayList<Carta>> getTaulerComu() {
         return taulerComu;
     }
 
-    public void setTaulerComu(ArrayList<ArrayList<Carta>> taulerComu) {
-        this.taulerComu = taulerComu;
-    }
-
     public Baralla getBaralla() {
         return baralla;
-    }
-
-    public void setBaralla(Baralla baralla) {
-        this.baralla = baralla;
     }
 
     public ArrayList<Carta> getPilarDescartades() {
         return pilarDescartades;
     }
 
-    public void setPilarDescartades(ArrayList<Carta> pilarDescartades) {
-        this.pilarDescartades = pilarDescartades;
+    public int getTorn() {
+        return tornActual;
     }
 }
